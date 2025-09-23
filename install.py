883b86e05f9beedd5963307ec9c75109bfbcd5d8
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-09-24 02:03:42 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
-# Tool Name :- Tool-X
-# Author :- Rajkumar dusad
-# Date :- 1/11/2017
+# Tool Name :- HatanToolX
+# Author :- hatanhack
+# Date :- 24/9/2025
 
 import os
 import sys
@@ -15,23 +15,23 @@ class tool:
       system=sys()
       os.system("clear")
       logo.ins_tnc()
-      inp=input("\033[1;33m Do you want to install Tool-X [Y/n]> \033[00m")
+      inp=input("\033[1;33m Do you want to install HatanToolX [Y/n]> \033[00m")
       if inp=="y" or inp=="Y":
         os.system("clear")
         logo.installing()
         if system.sudo is not None:
           #require root permission
-          if os.path.exists(system.conf_dir+"/Tool-X"):
+          if os.path.exists(system.conf_dir+"/HatanToolX"):
             pass
           else:
-            os.system(system.sudo+" mkdir "+system.conf_dir+"/Tool-X")
-          os.system(system.sudo+" cp -r modules core Tool-X.py "+system.conf_dir+"/Tool-X")
-          os.system(system.sudo+" cp -r core/Tool-X "+system.bin)
-          os.system(system.sudo+" cp -r core/toolx "+system.bin)
-          os.system(system.sudo+" chmod +x "+system.bin+"/Tool-X")
-          os.system(system.sudo+" chmod +x "+system.bin+"/toolx")
-          os.system("cd .. && "+system.sudo+" rm -rf Tool-X")
-          if os.path.exists(system.bin+"/Tool-X") and os.path.exists(system.conf_dir+"/Tool-X"):
+            os.system(system.sudo+" mkdir "+system.conf_dir+"/HatanToolX")
+          os.system(system.sudo+" cp -r modules core HatanToolX.py "+system.conf_dir+"/HatanToolX")
+          os.system(system.sudo+" cp -r core/HatanToolX "+system.bin)
+          os.system(system.sudo+" cp -r core/hatantoolx "+system.bin)
+          os.system(system.sudo+" chmod +x "+system.bin+"/HatanToolX")
+          os.system(system.sudo+" chmod +x "+system.bin+"/hatantoolx")
+          os.system("cd .. && "+system.sudo+" rm -rf HatanToolX")
+          if os.path.exists(system.bin+"/HatanToolX") and os.path.exists(system.conf_dir+"/HatanToolX"):
             os.system("clear")
             logo.ins_sc()
             tmp=input("\033[1;36m ##> \033[00m")
@@ -42,17 +42,17 @@ class tool:
             tmp=input("\033[1;36m ##> \033[00m")
             break
         else:
-          if os.path.exists(system.conf_dir+"/Tool-X"):
+          if os.path.exists(system.conf_dir+"/HatanToolX"):
             pass
           else:
-            os.system("mkdir "+system.conf_dir+"/Tool-X")
-          os.system("cp -r modules core Tool-X.py "+system.conf_dir+"/Tool-X")
-          os.system("cp -r core/Tool-X "+system.bin)
-          os.system("cp -r core/toolx "+system.bin)
-          os.system("chmod +x "+system.bin+"/Tool-X")
-          os.system("chmod +x "+system.bin+"/toolx")
-          os.system("cd .. && rm -rf Tool-X")
-          if os.path.exists(system.bin+"/Tool-X") and os.path.exists(system.conf_dir+"/Tool-X"):
+            os.system("mkdir "+system.conf_dir+"/HatanToolX")
+          os.system("cp -r modules core HatanToolX.py "+system.conf_dir+"/HatanToolX")
+          os.system("cp -r core/HatanToolX "+system.bin)
+          os.system("cp -r core/hatantoolx "+system.bin)
+          os.system("chmod +x "+system.bin+"/HatanToolX")
+          os.system("chmod +x "+system.bin+"/hatantoolx")
+          os.system("cd .. && rm -rf HatanToolX")
+          if os.path.exists(system.bin+"/HatanToolX") and os.path.exists(system.conf_dir+"/HatanToolX"):
             os.system("clear")
             logo.ins_sc()
             tmp=input("\033[1;36m ##> \033[00m")
@@ -62,12 +62,3 @@ class tool:
             logo.not_ins()
             tmp=input("\033[1;36m ##> \033[00m")
             break
-      else:
-        break
-
-if __name__=="__main__":
-  try:
-    tool.install()
-  except KeyboardInterrupt:
-    os.system("clear")
-    logo.exit()
