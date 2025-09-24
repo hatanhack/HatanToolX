af9e9069a3c03be21c259d02670470535358cffc
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-09-24 03:28:07 +0300

    Update and rename Tool-X.py to HatanToolX.py

--- HatanToolX.py
+++ HatanToolX.py
@@ -1,6 +1,11 @@
+# Tool Name :- HatanToolX
+# Author :- hatanhack
+# Date :- 24/9/2025
+
 import os
 import sys
 from modules.menu import *
+from modules.logo import *
 
 if __name__=="__main__":
   try:
