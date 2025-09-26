a4696ab22a57d447f5b69f6ba11dd12971165647
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-09-27 01:56:39 +0300

    Update HatanToolX.py

--- HatanToolX.py
+++ HatanToolX.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python3
 # Tool Name :- HatanToolX
-# Author :- hatanhack
-# Date :- 24/9/2025
+# Author :- hatanhack (modified)
+# Date :- 24/9/2025 (modified)
 
 import os
 import sys
 from modules.menu import *
 from modules.logo import *
 
-if __name__=="__main__":
-  try:
-    main.menu()
-  except KeyboardInterrupt:
-    os.system("clear")
-    logo.exit()
+if __name__ == "__main__":
+    try:
+        main.menu()
+    except KeyboardInterrupt:
+        os.system("clear")
+        try:
+            logo.exit()
+        except Exception:
+            # fallback safe exit
+            print("\nExiting...")
+            sys.exit(0)
